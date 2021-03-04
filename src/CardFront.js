de4daf2501464598012a7a9136721f4848358ed7
Elsa Fluss <elsafluss@gmail.com>
2021-03-03 17:08:45 -0700

    Remove unnecessary search file.

--- src/CardFront.js
+++ src/CardFront.js
@@ -10,21 +10,22 @@ class CardFront extends Component {
         if(this.props.playerData) {
             return (
                 <div>
-                <p>
-                    {this.props.playerData.FirstName} {this.props.playerData.LastName}
-                </p>
-                <p>
-                    {this.props.playerData.Jersey}
-                </p>
-                <p>
-                    {this.props.playerData.Position}
-                </p>
-                <img src={this.props.playerData.PhotoUrl} alt="the player"></img>
-            </div>
-        )
-    } else {
-        return null
-    }}
+                    <p>
+                        {this.props.playerData.FirstName} {this.props.playerData.LastName}
+                    </p>
+                    <p>
+                        {this.props.playerData.Jersey}
+                    </p>
+                    <p>
+                        {this.props.playerData.Position}
+                    </p>
+                    <img src={this.props.playerData.PhotoUrl} alt="the player"></img>
+                </div>
+            )
+        } else {
+            return null
+        }
+    }
 }
 
 export default CardFront
