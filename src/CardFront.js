74728a7991ac9792034056c390066f9c211e4968
Elsa Fluss <elsafluss@gmail.com>
2021-03-03 17:03:13 -0700

    Display minimal stats on card front when player is selected.

--- src/CardFront.js
+++ src/CardFront.js
@@ -3,24 +3,28 @@ import React, { Component } from 'react'
 class CardFront extends Component {
     constructor() {
         super()
-        this.state = {
-            playerData: {}
-        }
-    }
-
-    componentWillUnmount() {
-        this.setState({ playerData: this.props.playerData })
+        this.state = {}
     }
 
     render() {
-        console.log(this.props.playerData)
-        return (
-            <div>
-                {this.props.playerName}
-                {/* {this.props.playerData.FirstName} */}
+        if(this.props.playerData) {
+            return (
+                <div>
+                <p>
+                    {this.props.playerData.FirstName} {this.props.playerData.LastName}
+                </p>
+                <p>
+                    {this.props.playerData.Jersey}
+                </p>
+                <p>
+                    {this.props.playerData.Position}
+                </p>
+                <img src={this.props.playerData.PhotoUrl} alt="the player"></img>
             </div>
         )
-    }
+    } else {
+        return null
+    }}
 }
 
 export default CardFront
